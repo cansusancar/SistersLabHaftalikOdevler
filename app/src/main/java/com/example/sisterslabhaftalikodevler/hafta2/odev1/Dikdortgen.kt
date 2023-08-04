45bebc1ea6ab8a3c9d48fd6c09cdcb12f76c627c
Your Name <you@example.com>
2023-08-04 15:18:41 +0300

    committed04.08

--- app/src/main/java/com/example/sisterslabhaftalikodevler/hafta2/odev1/Dikdortgen.kt
+++ app/src/main/java/com/example/sisterslabhaftalikodevler/hafta2/odev1/Dikdortgen.kt
@@ -1,11 +1,11 @@
 package com.example.sisterslabhaftalikodevler.hafta2.odev1
 
 class Dikdortgen(val uzunluk: Double, val genislik: Double) : GeometrikSekil() {
-    override fun alanHesapla(): Double {
+   override fun alanHesapla(): Double {
         return uzunluk * genislik
     }
 
-    override fun cevreHesapla(): Double {
+ override fun cevreHesapla(): Double {
         return 2 * (uzunluk + genislik)
     }
 }
