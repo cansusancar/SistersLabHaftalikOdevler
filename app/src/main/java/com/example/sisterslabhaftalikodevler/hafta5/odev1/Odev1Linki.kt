5d1fee1f8a9551505d5fcc270fc294a54bcfcc26
Your Name <you@example.com>
2023-08-15 01:31:18 +0300

    committed15.08

---
+++ app/src/main/java/com/example/sisterslabhaftalikodevler/hafta5/odev1/Odev1Linki.kt
@@ -0,0 +1,10 @@
+package com.example.sisterslabhaftalikodevler.hafta5.odev1
+
+/*  Ödev 1: ImageView ve TextView içeren bir nesne sınıfı tanımlayın.
+      RecyclerView içerisinde 3 lü ve yatayda scroll edilebilecek şekilde kodlayınız.
+
+      projenin olduğu reponun github linki : 
+   https://github.com/cansusancar/SistersLabKotlinDersleriHafta5RecyclerViewOdevProje
+
+ */
+
