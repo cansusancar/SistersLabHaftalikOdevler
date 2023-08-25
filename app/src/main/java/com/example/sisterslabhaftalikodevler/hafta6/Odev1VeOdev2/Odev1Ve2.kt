24ec5d83ca5223cfa02345cdedabb8074c34b377
Win10 <dfdffdffd@>
2023-08-25 23:34:10 +0300

    committed25.08

---
+++ app/src/main/java/com/example/sisterslabhaftalikodevler/hafta6/Odev1VeOdev2/Odev1Ve2.kt
@@ -0,0 +1,3 @@
+package com.example.sisterslabhaftalikodevler.hafta6.Odev1VeOdev2
+
+// https://github.com/cansusancar/Week6Homework1AndHomework2
---
+++ app/src/main/java/com/example/sisterslabhaftalikodevler/hafta6/Odev3/Odev3.kt
@@ -0,0 +1,3 @@
+package com.example.sisterslabhaftalikodevler.hafta6.Odev3
+
+// https://medium.com/@cansusancar01/navigationcomponent-kullanımı-65026d87dd66
