83a222b451446aafe97eb26d26b902774687b8b3
cansusancar <116024772+cansusancar@users.noreply.github.com>
2023-09-08 23:54:59 +0300

    committed08.09

---
+++ app/src/main/java/com/example/sisterslabhaftalikodevler/hafta7/odev1/odev1link.kt
@@ -0,0 +1,9 @@
+package com.example.sisterslabhaftalikodevler.hafta7.odev1
+
+// odev 1 :Firebase Realtime Database kullanarak Notlar Uygulaması yapılacak
+// RecyclerViewde notlar listelenecek notlar silinebilmeli,
+// güncellenebilmeli ve ekleme yapıla bilmeli ( tasarımı ve renkler size özgün olmalı)
+
+
+//ODEV GİTHUB LİNKİ:
+//https://github.com/cansusancar/NotesApplication
---
+++ app/src/main/java/com/example/sisterslabhaftalikodevler/hafta7/odev2/odev2link.kt
@@ -0,0 +1,8 @@
+package com.example.sisterslabhaftalikodevler.hafta7.odev2
+
+// Odev 2 : .Medium yazısı yazılacak. Medium konusu Firebase Realtime ve Firestore farkı nedir .
+// Firebase Realtime Database ile veri ekleme silme güncelleme işlemleri nasıl yapılır
+
+
+// ODEV MEDİUM YAZISI LİNKİ :
+// https://medium.com/@cansusancar01/firebase-realtime-ve-firestore-farkı-nedir-61c29ec6b788
