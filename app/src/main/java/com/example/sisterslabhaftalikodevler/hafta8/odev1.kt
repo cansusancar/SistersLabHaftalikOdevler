1f27207b7396a73c66b1b68f0038c931cb73f364
cansusancar <116024772+cansusancar@users.noreply.github.com>
2023-09-15 18:41:58 +0300

    committed15.09

---
+++ app/src/main/java/com/example/sisterslabhaftalikodevler/hafta8/odev1.kt
@@ -0,0 +1,6 @@
+package com.example.sisterslabhaftalikodevler.hafta8
+
+
+// Ödevlerimde 1 hafta eksik gibi görünebilir fakat akademide İlk hafta ödev verilmediği için totalde 9 haftanın ödevi var projede
+// Medium yazısı linki:
+// https://medium.com/@cansusancar01/model-view-viewmodel-mvvm-teknolojisi-nedir-2577ed4721ee
---
+++ app/src/main/java/com/example/sisterslabhaftalikodevler/hafta8/odev2.kt
@@ -0,0 +1,4 @@
+package com.example.sisterslabhaftalikodevler.hafta8
+
+// cocukEglence uygulaması linki:
+// https://github.com/cansusancar/CocukEglence
